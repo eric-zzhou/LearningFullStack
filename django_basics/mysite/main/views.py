392a8437fcd797f6d27c40ef02baf67f0c107072
Eric Zhou <ericsuper@outlook.com>
2023-12-30 15:38:28 -0800

    Databases and models with Django

--- django_basics/mysite/main/views.py
+++ django_basics/mysite/main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TodoList, Item
 
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Testing Name</h1>")
-
-
-def v1(response):
-    return HttpResponse("<h1>view 1</h1>")
+def index(response, name):
+    ls = TodoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse(f"<h1>{ls.name}</h1></br><p>{item.text}<p>")
