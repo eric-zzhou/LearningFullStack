22613203d73833b81bb7f520c2bd5869ed820517
Eric Zhou <ericsuper@outlook.com>
2023-12-30 20:01:52 -0800

    Bootstrap integration with django

--- django_basics/mysite/main/forms.py
+++ django_basics/mysite/main/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class CreateNewList(forms.Form):
-    name = forms.CharField(label="Name", max_length=200)
-    check = forms.BooleanField(required=False)
+    name = forms.CharField()
+    # check = forms.BooleanField(required=False)
