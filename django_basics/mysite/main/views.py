5d74ad44e6a9dd01d2f41c9161ef859d9474f2c5
Eric Zhou <ericsuper@outlook.com>
2023-12-30 16:32:04 -0800

    Jinja and HTML templates in django

--- django_basics/mysite/main/views.py
+++ django_basics/mysite/main/views.py
@@ -4,7 +4,11 @@ from .models import TodoList, Item
 
 
 # Create your views here.
-def index(response, name):
-    ls = TodoList.objects.get(name=name)
-    item = ls.item_set.get(id=1)
-    return HttpResponse(f"<h1>{ls.name}</h1></br><p>{item.text}<p>")
+def index(response, id):
+    ls = TodoList.objects.get(id=id)
+    # item = ls.item_set.get(id=1)
+    return render(response, "main/list.html", {"ls": ls})
+
+
+def home(response):
+    return render(response, "main/home.html", {"name": "home"})
--- django_basics/mysite/mysite/__init__.py
+++ django_basics/mysite/mysite/__init__.py
@@ -1,5 +1,15 @@
+# PROJECT SETUP:
 # django-admin startproject SITE_NAME
 # cd SITE_NAME
 # python manage.py startapp APP_NAME
 
+# CREATE ADMIN:
+# python manage.py createsuperuser
+
+# SQL MODIFICATION AND MIGRATIONS https://docs.djangoproject.com/en/5.0/topics/migrations/#:~:text=Migrations%20are%20Django's%20way%20of,problems%20you%20might%20run%20into.
+# python manage.py shell
+# python manage.py migrate
+# python manage.py makemigrations main
+
+# RUN SERVER:
 # python manage.py runserver
