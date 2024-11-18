35bf0f4266b37f18a56b35c81345cba66e9edb46
eric-zzhou <ericsuper@outlook.com>
2024-11-18 06:02:06 -0500

    Finished basic thing

--- fastapi_postgres/main.py
+++ fastapi_postgres/main.py
@@ -30,6 +30,26 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+@app.get("/questions/{question_id}")
+async def read_question(question_id: int, db: db_dependency):
+    result = (
+        db.query(models.Questions).filter(models.Questions.id == question_id).first()
+    )
+    if not result:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return result
+
+
+@app.get("/choices/{question_id}")
+async def read_choices(question_id: int, db: db_dependency):
+    result = (
+        db.query(models.Choices).filter(models.Choices.question_id == question_id).all()
+    )
+    if not result:
+        raise HTTPException(status_code=404, detail="Choices not found")
+    return result
+
+
 @app.post("/questions/")
 async def create_questions(question: QuestionBase, db: db_dependency):
     db_question = models.Questions(question_text=question.question_text)
@@ -38,7 +58,7 @@ async def create_questions(question: QuestionBase, db: db_dependency):
     db.refresh(db_question)
     for choice in question.choices:
         db_choice = models.Choices(
-            choice_test=choice.choice_text,
+            choice_text=choice.choice_text,
             is_correct=choice.is_correct,
             question_id=db_question.id,
         )
